a017fee718b77a02c3443d3409100ab55b645e75
Thanadon99 <38921597+Thanadon99@users.noreply.github.com>
2018-05-03 13:15:44 +0700

    แก้

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'kCYCcusinQGAJK1hGsJMxfz+yCXIYqUU0R3T/G/6cakALD65s2ScD22xCaUoD0unEmFAjl7gvElqWawH4o7AUJxGnKbhpogowCJqIA1cQ57gIwNKiJ1zdfpIqm+eZOOaQJeCCSUOmSPX0zvWFoDCNAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
